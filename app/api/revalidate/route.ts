697b96f7d9eb905f61bcb89bed0b710be495bbaf
Nikola Mirilo <nikolamirilo@gmail.com>
2025-10-19 21:24:21 +0200

    added revalidate feature

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,22 @@
+import { revalidateData } from "@/helpers/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+	try {
+		await revalidateData();
+		return new Response(
+			JSON.stringify({ message: "Data revalidated successuflly" }),
+			{
+				status: 200,
+				headers: { "Content-Type": "application/json" },
+			},
+		);
+	} catch (error: any) {
+		console.error("Request error:", error);
+		return new Response(JSON.stringify({ error: error.message }), {
+			status: 400,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+}
