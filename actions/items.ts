e85168df3a4279b76c5c5f96708e6312d4ef79eb
Nikola Mirilo <nikolamirilo@gmail.com>
2025-10-21 20:31:34 +0200

    updated items

--- actions/items.ts
+++ actions/items.ts
@@ -1,8 +1,8 @@
 "use server";
+import { generateUniqueSlug } from "@quicktalog/common";
 import { revalidateData } from "@/helpers/server";
 import { Status } from "@/types/enums";
 import { createClient } from "@/utils/supabase/server";
-import { generateUniqueSlug } from "@quicktalog/common";
 
 export async function deleteItem(id: string): Promise<boolean> {
 	try {
