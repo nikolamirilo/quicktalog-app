2eb5f79c3e7877bc0f184039376cf6a236ff3be0
Nikola Mirilo <nikolamirilo@gmail.com>
2025-11-11 11:55:54 +0100

    updated dashboard userData retreival

--- app/admin/dashboard/[[...rest]]/page.tsx
+++ app/admin/dashboard/[[...rest]]/page.tsx
@@ -1,3 +1,4 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { UserData } from "@quicktalog/common";
 import Link from "next/link";
 import { getUserData } from "@/actions/users";
@@ -11,8 +12,11 @@ import type { AreLimitesReached } from "@/types";
 export const dynamic = "force-dynamic";
 
 export default async function page() {
-	const userData: UserData = await getUserData();
-
+	const clerkUser = await currentUser();
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${clerkUser.id}`,
+	);
+	const userData: UserData = await res.json();
 	if (!userData) {
 		return (
 			<div className="product font-lora min-h-screen">
